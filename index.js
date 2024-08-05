475fe66b85af195233946b79be589e556d618c01
Gabrielrolonmatheus <129123616+Gabrielrolonmatheus@users.noreply.github.com>
2024-08-05 23:01:16 +0000

    05/08/24

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const meuNome = "Gabriel Matheus";
 const idade = 21;
 console.log(meuNome);
 console.log(idade);
-const altura = 1.82
-const peso = 85
+const altura = 1.82;
+const peso = 85;
 console.log(altura);
-const cidade = "São José dos Pinhais"
+const cidade = "São José dos Pinhais";
 console.log(cidade);
 
 
