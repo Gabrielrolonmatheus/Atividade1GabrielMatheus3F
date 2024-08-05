310284afb948b293a26af5578e3e15d1a17d47c2
Gabrielrolonmatheus <129123616+Gabrielrolonmatheus@users.noreply.github.com>
2024-08-05 22:52:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const meuNome = "Gabriel Matheus";
+const idade = 21;
+console.log(meuNome);
+console.log(idade);
+const altura = 1.82
+const peso = 85
+console.log(altura);
+const cidade = "São José dos Pinhais"
+console.log(cidade);
+
+
+
+
+
